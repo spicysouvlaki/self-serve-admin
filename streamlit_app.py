75fb423d0b00fba1fe2ba22c99cbbbc177b344a6
Matteo Monchiero <matteo@streamlit.io>
2021-03-17 15:11:29 -0400

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def get_roles(user_id):
     except Exception as err:
         st.write(f'Other error occurred: {err}')
     else:
-        st.write('Success!', r)
+        st.write('Success!', r.json())
 
     return ["FAKE_ROLE1", "FAKE_ROLE2"]
 
