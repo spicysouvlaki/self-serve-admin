4927dd4336635c7c9540f931b7532cd1ea54ac02
Matteo Monchiero <matteo@streamlit.io>
2021-03-17 15:47:13 -0400

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def get_roles(user_id):
 
 def add_role(user_id, role):
     try:
-        r = requests.post(f"http://apps-manager:8500/http/add-user-role", data={'github_user_id': user_id, "permission": 4})
+        r = requests.post(f"http://apps-manager:8500/http/add-user-role", json={'github_user_id': user_id, "permission": 4})
         st.write(r.request.body)
         r.raise_for_status()
     except HTTPError as http_err:
@@ -46,7 +46,7 @@ roles_map = {
 
 def delete_role(user_id, role):
     try:
-        r = requests.post(f"http://apps-manager:8500/http/delete-user-role", data={'github_user_id': user_id, "permission": 4})
+        r = requests.post(f"http://apps-manager:8500/http/delete-user-role", json={'github_user_id': user_id, "permission": 4})
         r.raise_for_status()
     except HTTPError as http_err:
         st.write(f'HTTP error occurred: {http_err}')
